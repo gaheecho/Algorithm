624aaa6d423fde95cd4ade86f5748883675370ac
gaheeCho <gahee.cho@traport.com>
2016-12-07 19:00:44 +0900

    주석제거

--- #1.js
+++ #1.js
@@ -2,26 +2,6 @@
   가장 얇은 지갑 만들기
 */
 
-// let makeThinWallet = (inputPrice) => {
-//   let price_170000 = 0, price_110000 = 0, price_70000 = 0, price_10000 = 0;
-//   while(inputPrice > 0){
-//     if(inputPrice >= 170000) {
-//       price_170000++;
-//       inputPrice = inputPrice - 170000;
-//     } else if(inputPrice >= 110000) {
-//       price_110000++;
-//       inputPrice = inputPrice - 110000;
-//     } else if(inputPrice >= 70000) {
-//       price_70000++;
-//       inputPrice = inputPrice - 70000;
-//     } else if(inputPrice >= 10000) {
-//       price_10000++;
-//       inputPrice = inputPrice - 10000;
-//     }
-//   }
-//   return '17만원 :' + price_170000 + '개, 11만원: ' + price_110000 + '개, 7만원 : ' + price_70000 + '개, 1만원: ' + price_10000 + '개';
-// }
-
 let paper = [17, 11, 7, 1];
 
 let makeThinWallet = (inputPaper) => {
